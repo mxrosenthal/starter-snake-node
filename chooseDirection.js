a8dea871b1086299622c3643db3b723395bd27d0
Jon Langlois <jonathan-langlois@live.ca>
2020-03-11 11:04:44 -0700

    cleaning

--- chooseDirection.js
+++ chooseDirection.js
@@ -1,20 +1,4 @@
 const chooseDirection = function(body, directions) {
-  // const rise = closestMealArray[0].y - headPosition.y;
-  // const run = closestMealArray[0].x - headPosition.x;
-
-  // if (Math.abs(rise) >= Math.abs(run) && rise > 0) {
-  //   moveList.push('down');
-  //   return 'down';
-  // } else if (Math.abs(rise) >= Math.abs(run) && rise < 0) {
-  //   moveList.push('up');
-  //   return 'up';
-  // } else if (Math.abs(rise) <= Math.abs(run) && run > 0) {
-  //   moveList.push('right');
-  //   return 'right';
-  // } else if (Math.abs(rise) <= Math.abs(run) && run < 0) {
-  //   moveList.push('left');
-  //   return 'left';
-  // }
   let direction = null;
   if (body.you.health > 50){
     let minDanger;
@@ -36,7 +20,6 @@ const chooseDirection = function(body, directions) {
       }
     });
   }
-  console.log(direction)
   switch (direction) {
     case 0:
       return "up"
--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const getDanger = require('./getDanger.js');
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
 app.set('port', process.env.PORT || 9001);
-// app.set('port', 9001);
 app.enable('verbose errors');
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,41 +40,26 @@ app.post('/start', (request, response) => {
   return response.json(data);
 });
 
+//test route you can mess around with 
 app.post('/test', (request, response) => {
   // NOTE: Do something here to start the game
   const info = request.body;
   // Response data
-
   const directions = rankFood(info.you.body[0], info.board.food, info);
-
   const dangerDirections = getDanger(info, directions, getOpenSquares(info));
-
   console.log(dangerDirections)
   const direction = chooseDirection(info, dangerDirections);
-
-
   return response.json(direction);
 });
 
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
-  // NOTE: Do something here to generate your move
-  // const info = request.body;
-  // const head = info.you.body[0];
-  // const foodArray = info.board.food;
-  // const hunger = info.you.health < 40;
-  // const possibleMoves = getOpenSquares(info);
-  // let rankedMoves = rankFood(head, foodArray, possibleMoves);
-  // rankedMoves = rankDanger(info, rankedMoves);
-  // const move = chooseDirection(rankedMoves, hunger);
-
   const info = request.body;
-  // Response data
-
+  //rank food returns the [{N}, {S}, {E}, {W}] array with food keys
   const directions = rankFood(info.you.body[0], info.board.food, info);
-
+  //adds danger keys to each direction
   const dangerDirections = getDanger(info, directions, getOpenSquares(info));
-
+  //returns "up", "down", etc.
   const direction = chooseDirection(info, dangerDirections);
   const data = { move:direction };
   return response.json(data);
